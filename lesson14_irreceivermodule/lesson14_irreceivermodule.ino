d1a5a3cc6f1744ef785658b8b563c937f9dd1717
Cédric Sauriol <cedric.sauriol@hotmail.com>
2019-10-26 16:16:34 -0400

    Completed lesson 14 with an integrated HashMap.

--- lesson14_irreceivermodule/lesson14_irreceivermodule.ino
+++ lesson14_irreceivermodule/lesson14_irreceivermodule.ino
@@ -1,5 +1,6 @@
 #include "IRremote.h"
 #include "RemoteKeys.h"
+#include "hashmap.h"
 
 #define BAUD_RATE 9600
 #define RECEIVER_PIN 11
@@ -7,47 +8,76 @@
 IRrecv receiver(RECEIVER_PIN);
 decode_results results;
 
+/**
+ * Hash map elements.
+ */
+class H : public KeyHash<uint32_t> {
+    public:
+        // https://github.com/skeeto/hash-prospector
+        uint32_t operator() (uint32_t key) {
+            key ^= key >> 17;
+            key *= 0xed5ad4bb;
+            key ^= key >> 11;
+            key *= 0xac4c1b51;
+            key ^= key >> 15;
+            key *= 0x31848bab;
+            key ^= key >> 14;
+            return key;
+        }
+};
+
+class C : public KeyComparator<uint32_t> {
+    public:
+        bool operator() (uint32_t keyA, uint32_t keyB) {
+            return keyA == keyB;
+        }
+};
+
+HashMap<uint32_t, String, H, C>* hmap = new HashMap<uint32_t, String, H, C>(31);
+
+/**
+ * Methods.
+ */ 
 void setup() {
     Serial.begin(BAUD_RATE);
     receiver.enableIRIn();
+
+    hmap->put(BTN_POWER, "POWER");
+    hmap->put(BTN_FUNC_STOP, "FUNC/STOP");
+    hmap->put(BTN_VOL_UP, "VOL+");
+    hmap->put(BTN_REWIND, "FAST BACK");
+    hmap->put(BTN_PAUSE, "PAUSE");
+    hmap->put(BTN_FAST_FORWARD, "FAST FORWARD");
+    hmap->put(BTN_DN, "DOWN");
+    hmap->put(BTN_VOL_DN, "VOL-");
+    hmap->put(BTN_UP, "UP");
+    hmap->put(BTN_EQ, "EQ");
+    hmap->put(BTN_ST_REPT, "ST/REPT");
+    hmap->put(BTN_0, "0");
+    hmap->put(BTN_1, "1");
+    hmap->put(BTN_2, "2");
+    hmap->put(BTN_3, "3");
+    hmap->put(BTN_4, "4");
+    hmap->put(BTN_5, "5");
+    hmap->put(BTN_6, "6");
+    hmap->put(BTN_7, "7");
+    hmap->put(BTN_8, "8");
+    hmap->put(BTN_9, "9");
+    hmap->put(REPEAT, "REPEAT"); 
 }
 
 void loop() {
     if(receiver.decode(&results)) {
-        unsigned long signal = results.value;
+        uint32_t signal = results.value;
         logSignal(signal);
         receiver.resume();
     }
-
 }
 
-void logSignal(unsigned long value) {
-    switch(value) {
-        case BTN_POWER:         Serial.println("POWER");        break;
-        case BTN_FUNC_STOP:     Serial.println("FUNC/STOP");    break;
-        case BTN_VOL_UP:        Serial.println("VOL+");         break;
-        case BTN_REWIND:        Serial.println("FAST BACK");    break;
-        case BTN_PAUSE:         Serial.println("PAUSE");        break;
-        case BTN_FAST_FORWARD:  Serial.println("FAST FORWARD"); break;
-        case BTN_DN:            Serial.println("DOWN");         break;
-        case BTN_VOL_DN:        Serial.println("VOL-");         break;
-        case BTN_UP:            Serial.println("UP");           break;
-        case BTN_EQ:            Serial.println("EQ");           break;
-        case BTN_ST_REPT:       Serial.println("ST/REPT");      break;
-        case BTN_0:             Serial.println("0");            break;
-        case BTN_1:             Serial.println("1");            break;
-        case BTN_2:             Serial.println("2");            break;
-        case BTN_3:             Serial.println("3");            break;
-        case BTN_4:             Serial.println("4");            break;
-        case BTN_5:             Serial.println("5");            break;
-        case BTN_6:             Serial.println("6");            break;
-        case BTN_7:             Serial.println("7");            break;
-        case BTN_8:             Serial.println("8");            break;
-        case BTN_9:             Serial.println("9");            break;
-        case REPEAT:            Serial.println("REPEAT");       break;  
-
-        default: Serial.println("Unknown signal.");
-    }
+void logSignal(uint32_t value) {
+    String msg = "Unknown signal.";
+    hmap->get(value, &msg);
+    Serial.println(msg);
 
     // Prevent immediate repeats.
     delay(500);
